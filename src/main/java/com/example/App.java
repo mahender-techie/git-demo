71871698c4cf5be2944eb79311f7ba3c55aa729a
mahender-techie <mahi.techie001@gmail.com>
2019-04-20 13:43:55 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	public void addData(String data) {
+		System.out.println("Data is :"+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
