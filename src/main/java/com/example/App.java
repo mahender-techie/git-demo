0dfe9d09b1c25c55b6f6dfb27188dcf504068f8f
mahender-techie <mahi.techie001@gmail.com>
2019-04-20 14:23:43 +0530

    modified addData method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,6 +10,9 @@ public class App
 	
 	public void addData(String data) {
 		System.out.println("Data is :"+data);
+		if(data.equalsIgnoreCase("java")) {
+			System.out.println("Welcome");
+		}
 	}
     public static void main( String[] args )
     {
